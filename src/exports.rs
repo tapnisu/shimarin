5b2f1f280ea692b1c2637fc2b583ffc167b7e860
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-26 23:13:07 +0700

    split into multiple types

---
+++ src/exports.rs
@@ -0,0 +1,4 @@
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
+// User data, which is stored and accessible in all command invocations
+pub struct Data {}
